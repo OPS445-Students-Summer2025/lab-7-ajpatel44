cf72160f509848c44a343f3e28e4ca2718f1a5fe
Aryan Patel <ajpatel44@myseneca.ca>
2025-07-18 15:33:47 -0400

    Added lab7a

--- lab7a.py
+++ lab7a.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Student ID: ajpatel44
+
 class Time:
     """Simple object type for time of the day.
     data attributes: hour, minute, second
     """
-    def __init__(self,hour=12,minute=0,second=0):
-        """constructor for time object""" 
+    def __init__(self, hour=12, minute=0, second=0):
+        """constructor for time object"""
         self.hour = hour
         self.minute = minute
         self.second = second
@@ -15,16 +16,16 @@ def format_time(t):
     return f'{t.hour:02d}:{t.minute:02d}:{t.second:02d}'
 
 def sum_times(t1, t2):
-    """Add two time objests and return the sum."""
-    sum = Time(0,0,0)
-    sum.hour = t1.hour + t2.hour
-    sum.minute = t1.minute + t2.minute
-    sum.second = t1.second + t2.second
-    return sum
+    """Add two time objects and return the sum."""
+    total_seconds = (t1.hour * 3600 + t1.minute * 60 + t1.second) + (t2.hour * 3600 + t2.minute * 60 + t2.second)
+    hour, remainder = divmod(total_seconds, 3600)
+    minute, second = divmod(remainder, 60)
+    hour = hour % 24
+    return Time(hour, minute, second)
 
 def valid_time(t):
     """check for the validity of the time object attributes:
-        24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
+       24 > hour >= 0, 60 > minute >= 0, 60 > second >= 0 """
     if t.hour < 0 or t.minute < 0 or t.second < 0:
         return False
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
